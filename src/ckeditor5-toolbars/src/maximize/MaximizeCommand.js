bdc31b89ef9d2de11fe5d52c46852445927dab33
Amit Dubey <akd6882@gmail.com>
2018-10-16 17:30:17 +0530

    Update MaximizeCommand.js

--- src/ckeditor5-toolbars/src/maximize/MaximizeCommand.js
+++ src/ckeditor5-toolbars/src/maximize/MaximizeCommand.js
@@ -25,7 +25,7 @@ export default class MaximizeCommand extends Command {
 				this.e.sourceElement.nextSibling.classList.add("ckeditorfullsize");
 				 var heightvalue = window.innerHeight - this.e.sourceElement.nextSibling.children[1].offsetHeight - 2;
 				 var stylevalue= "height: " + heightvalue + "px;";
-				 var fullstyle="display: block; z-index: 999; position: absolute; left: 0px; top: 0px; width: " +  window.innerWidth + "px;";
+				 var fullstyle="display: block; z-index: 999; position: absolute; left: 0px; top: 0px; width: 100%;";
 				 this.e.sourceElement.nextSibling.style=fullstyle;
 				 this.e.sourceElement.nextSibling.children[2].children[0].style=stylevalue;
 			}
